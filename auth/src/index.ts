bbac965bca8357479c61c2ddb49ca626a119926d
Artur Silva <arqueiroz@deloitte.pt>
2022-02-15 14:49:18 +0000

    auth routes and error handle

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,21 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 
 import { authRoutes } from './routes/auth-routes';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
 
 app.use(authRoutes);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000!!!!!!!!');
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 const router = express.Router();
+
+//router.post('/api/users/signup', [ validation Here ] async (req, res) => {
 
 router.post(
   "/api/users/signup",
@@ -12,21 +16,18 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
 
-    console.log('signup...')
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new RequestValidationError(errors.array());
     }
-    const { email, password } = req.body;
 
     console.log("Creating a user...");
+    throw new DatabaseConnectionError();
 
     res.send({});
-
-    // new User({ email, password })
   }
 );
 
